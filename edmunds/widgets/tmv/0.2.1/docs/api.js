3a0bdaf4ff87e48b912bd0f2eb862d060158baf9
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2014-02-10 18:52:51 +0300

    added tmv widget v0.2.1

---
+++ edmunds/widgets/tmv/0.2.1/docs/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EDM.Observable",
+        "EDM.TMV",
+        "EDM.Util",
+        "EDM.Widget"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
