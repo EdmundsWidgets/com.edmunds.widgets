85a952be59556e0a03cbb4e670de3036a2c45967
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2014-02-10 16:19:11 +0300

    added nvc loader

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ exports.index = function(req, res){
     res.render('index', { title: 'Edmunds Widgets' });
 };
 
+exports.loader = function(req, res){
+    res.setHeader('Content-Type', 'text/javascript');
+    res.render('loader', {
+        baseUrl: req.protocol + '://' + req.headers.host
+    });
+};
+
 exports.error404 = function(req, res){
     res.status(404).render('404');
 };
