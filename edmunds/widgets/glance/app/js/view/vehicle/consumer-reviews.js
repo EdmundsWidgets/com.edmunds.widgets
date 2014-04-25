edc17b1a0269361f6da054a0a2d0fad227f7b1e7
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-04-25 16:52:07 +0300

    Update version of the consumer reviews tab

--- edmunds/widgets/glance/app/js/view/vehicle/consumer-reviews.js
+++ edmunds/widgets/glance/app/js/view/vehicle/consumer-reviews.js
@@ -1,11 +1,14 @@
 define([
     'template/vehicle/consumer-reviews',
-    'template/vehicle/consumer-reviews-rating'
-], function(consumerReviewsTemplate, consumerReviewsRatingTemplate) {
+    'template/vehicle/consumer-reviews-rating',
+    'model/vehicle/consumer-review'
+], function(consumerReviewsTemplate, consumerReviewsRatingTemplate, ConsumerReviewModel) {
     return Backbone.View.extend({
         template: consumerReviewsTemplate,
+        model: new ConsumerReviewModel(),
         ratingTemplate: consumerReviewsRatingTemplate,
         initialize: function() {
+            this.model.fetch();
         },
         render: function() {
             this.$container = $('.content');
@@ -13,8 +16,19 @@ define([
             this.$widget = $('.edm-widget');
             this.$widget.removeClass('edmunds-says');
             this.$widget.addClass('reviews-tab');
-            this.$ratingBar.html(this.ratingTemplate);
-            this.$container.html(this.template);
+            this.$ratingBar.html(this.ratingTemplate(this.model.toJSON()));
+            this.$ratingStars = $('.star');
+            this.$averageRating = this.model.get('averageRating');
+            this.$reviews = this.model.get('reviews');
+            this.$ratingStars.removeClass('filled');
+            _.each(this.$ratingStars, function(el, i) {
+                if (i < this.$averageRating) {
+                    $(el).addClass('filled');
+                }
+            }, this);
+            this.$container.html(this.template({
+                collection: this.$reviews
+            }));
             return this;
         }
     });
