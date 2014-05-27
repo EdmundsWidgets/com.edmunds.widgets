7399e4d14c7ada03677dc1ce56badf3c0d58554a
Pasha Karpovich <mr.jakebluess@gmail.com>
2014-05-27 14:21:07 +0300

    Update version of the AtGlance widget

--- edmunds/widgets/glance/app/js/app.js
+++ edmunds/widgets/glance/app/js/app.js
@@ -1,94 +1,59 @@
 define([
-    'view/vehicle/styles',
-    'view/vehicle/grade',
-    'view/vehicle/rating-bar',
-    'view/vehicle/edmunds-says',
-    'template/vehicle/header',
-    'template/vehicle/footer',
-    'view/vehicle/consumer-reviews'
-], function(StylesView, GradeView, RatingBarView, EdmundsSaysView, headerTemplate, footerTemplate, ConsumerReviewsView) {
-
+    'template/base/base',
+    'view/base/styles',
+    'view/rating-tab/rating-tab',
+    'view/edmunds-says-tab/edmunds-says-tab',
+    'view/consumer-reviews-tab/consumer-reviews-tab'
+], function(baseTemplate, StylesView, RatingTabView, EdmundsSaysTabView, ConsumerReviewsTabView) {
     return Backbone.View.extend({
-
-        className: 'edm-widget rating-tab',
-
+        className: 'edm-widget',
+        template: baseTemplate,
         events: {
-            'click .edm-navigation a[data-id="rating"]': 'ratingTab',
-            'click .edm-navigation a[data-id="edmunds-says"]': 'edmundsSays',
-            'click .edm-navigation a[data-id="consumer-reviews"]': 'consumerReviews'
+            'click a[data-id="rating-tab"]': 'ratingTab',
+            'click a[data-id="edmunds-says-tab"]': 'edmundsSaysTab',
+            'click a[data-id="consumer-reviews-tab"]': 'consumerReviewsTab'
         },
-
         initialize: function(options) {
-            this.options = options;
-            this.initializeStylesView(options);
-//            this.initializeGradeView(options);
-            this.initializeRatingBarView(options);
-            this.consumerReviewsView = new ConsumerReviewsView();
-        },
-
-        initializeStylesView: function(options) {
             this.stylesView = new StylesView({
                 apiKey: options.apiKey,
                 submodel: options.submodel
             });
-            this.listenTo(this.stylesView, 'change', this.onVehicleStyleChange);
-        },
-
-        /*initializeGradeView: function(options) { note: Delete this later
-            this.gradeView = new GradeView({
+            this.ratingTabView = new RatingTabView({
                 apiKey: options.apiKey
             });
-        },*/
-
-        initializeRatingBarView: function(options) {
-            this.ratingBarView = new RatingBarView({
+            this.edmundsSaysTabView = new EdmundsSaysTabView({
+                el: this.ratingTabView.el,
                 apiKey: options.apiKey
             });
+            this.consumerReviewsTabView = new ConsumerReviewsTabView({
+                el: this.ratingTabView.el,
+                apiKey: options.apiKey
+            });
+            this.edmundsSaysTabView.load();
+            this.render();
         },
-
         render: function() {
-            this.$el.append(headerTemplate);
-            this.$el.append(this.ratingBarView.el);
-            this.$('.list-style-id').append(this.stylesView.el);
-            this.$el.append(footerTemplate);
-//            this.$el.append(this.gradeView.el);
-            this.edmundsSaysView = new EdmundsSaysView({
-                el: '.content'
-            });
-            return this;
+            this.$el.html(this.template);
+            this.stylesView.setElement(this.$('.list-style-id'));
+            this.$('header').after(this.ratingTabView.el);
         },
-
-        onVehicleStyleChange: function(/*style*/) {
-            var styleId = 200434856; // style.get('id');
-//            this.gradeView.trigger('setStyleId', styleId);
-            this.ratingBarView.trigger('setStyleId', styleId);
-        },
-
-        edmundsSays: function(e) {
-            e.preventDefault();
-            this.$('.edm-navigation li').removeClass('active');
-            $(e.currentTarget).parent('li').addClass('active');
-            this.edmundsSaysView.render();
-        },
-
         ratingTab: function(e) {
             e.preventDefault();
-            this.$('.edm-navigation li').removeClass('active');
+            this.$('.edm-navigation').children('li').removeClass('active');
             $(e.currentTarget).parent('li').addClass('active');
-            this.$('.content').empty();
-            this.$el.removeClass('edmunds-says');
-            this.$el.removeClass('reviews-tab');
-            this.$el.addClass('rating-tab');
-            this.ratingBarView.render();
-            this.ratingBarView.gradeView.render();
+            this.ratingTabView.render();
         },
-        consumerReviews: function(e) {
+        edmundsSaysTab: function(e) {
             e.preventDefault();
-            this.$('.edm-navigation li').removeClass('active');
+            this.$('.edm-navigation').children('li').removeClass('active');
             $(e.currentTarget).parent('li').addClass('active');
-            this.consumerReviewsView.render();
+            this.edmundsSaysTabView.render();
+        },
+        consumerReviewsTab: function(e) {
+            e.preventDefault();
+            this.$('.edm-navigation').children('li').removeClass('active');
+            $(e.currentTarget).parent('li').addClass('active');
+            this.consumerReviewsTabView.render();
         }
-
     });
-
 });
--- edmunds/widgets/glance/app/js/model/vehicle/consumer-review.js
+++
@@ -1,9 +0,0 @@
-define(function() {
-    return Backbone.Model.extend({
-        url: 'https://api.edmunds.com/api/vehiclereviews/v2/styles/101403692?fmt=json&api_key=axr2rtmnj63qsth3ume3tv5f',
-        parse: function(response) {
-            response.averageRating = Math.round(response.averageRating).toFixed(1);
-            return response;
-        }
-    });
-});
